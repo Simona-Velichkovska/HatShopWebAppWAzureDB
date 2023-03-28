f58a7d253530f1fa8535311fd8d73a8b36043458
Simona Velichkovska <moni96girl@hotmail.com>
2023-03-28 18:07:27 +0200

    Add project files.

---
+++ Data/HatShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HatShopWebAppWAzureDB.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HatShopWebAppWAzureDB.Data
+{
+    public class HatShopDbContext : DbContext
+    {
+        public HatShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Hat> Hats { get; set; }
+
+
+
+    }
+}
