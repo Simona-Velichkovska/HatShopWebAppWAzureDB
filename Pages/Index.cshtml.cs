7559fbf66064200661b0bb393726eb5e35aa169c
Simona Velichkovska <moni96girl@hotmail.com>
2023-03-30 18:21:40 +0200

    Front page listing of the items and looking at the details of any item in that list.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HatShopWebAppWAzureDB.Models.Domain;
+using HatShopWebAppWAzureDB.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HatShopWebAppWAzureDB.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<Hat> Hats { get; set; }
+
+        private readonly IHatRepository HatRepository;
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHatRepository HatRepository)
         {
             _logger = logger;
+            this.HatRepository = HatRepository;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            Hats = (List<Hat>)await HatRepository.GetAllAsync();
         }
     }
 }
